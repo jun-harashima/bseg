b1c1818cedcb98a2098456f6eb56a07a76fd3075
Jun Harashima <jun-harashima@cookpad.com>
2018-11-19 20:55:50 +0900

    add ispredicate()

--- bseg/bunsetsu.py
+++ bseg/bunsetsu.py
@@ -1,4 +1,9 @@
 class Bunsetsu:
 
     def __init__(self, morps):
+        self.morphologies = morps
         self.surface = "".join([morp.surface for morp in morps])
+
+    def ispredicate(self):
+        return any([morp.part_of_speech in ["動詞", "形容詞", "判定詞"]
+                    for morp in self.morphologies])
--- tests/test_bunsetsu.py
+++ tests/test_bunsetsu.py
@@ -5,11 +5,21 @@ from bseg.bunsetsu import Bunsetsu
 
 class TestBunsetsu(unittest.TestCase):
 
+    def setUp(self):
+        morp1 = Morphology("天気	名詞,一般,*,*,*,*,天気,テンキ,テンキ")
+        morp2 = Morphology("が	助詞,格助詞,一般,*,*,*,が,ガ,ガ")
+        self.bnst1 = Bunsetsu([morp1, morp2])
+        morp3 = Morphology("良い	形容詞,自立,*,*,形容詞・アウオ段,\
+                           基本形,良い,ヨイ,ヨイ")
+        morp4 = Morphology("。	記号,句点,*,*,*,*,。,。,。")
+        self.bnst2 = Bunsetsu([morp3, morp4])
+
     def test___init__(self):
-        morp1 = Morphology("今日	名詞,副詞可能,*,*,*,*,今日,キョウ,キョー")
-        morp2 = Morphology("は	助詞,係助詞,*,*,*,*,は,ハ,ワ")
-        bnst = Bunsetsu([morp1, morp2])
-        self.assertEqual(bnst.surface, "今日は")
+        self.assertEqual(self.bnst1.surface, "天気が")
+
+    def test_ispredicate(self):
+        self.assertFalse(self.bnst1.ispredicate())
+        self.assertTrue(self.bnst2.ispredicate())
 
 
 if __name__ == "__main__":
