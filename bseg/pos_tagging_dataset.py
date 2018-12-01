0c5c6af9beaaf8dfb58d8c354d8dbc30aa32783b
Jun Harashima <jun-harashima@cookpad.com>
2018-12-01 12:40:03 +0900

    add PosTaggingDataset

---
+++ bseg/pos_tagging_dataset.py
@@ -0,0 +1,41 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class PosTaggingDataset(Dataset):
+
+    def __init__(self, examples):
+        self.word_to_index, self.tag_to_index = self._make_index(examples)
+        self.degitized_examples = self._degitize_all(examples)
+
+    def __len__(self):
+        return len(self.degitized_examples)
+
+    def __getitem__(self, i):
+        return self.degitized_examples[i]
+
+    def _make_index(self, examples):
+        word_to_index = {}
+        tag_to_index = {}
+        for (words, tags) in examples:
+            for word in words:
+                self._add_index(word, word_to_index)
+            for tag in tags:
+                self._add_index(tag, tag_to_index)
+        return word_to_index, tag_to_index
+
+    def _add_index(self, token, token_to_index):
+        if token not in token_to_index:
+            token_to_index[token] = len(token_to_index)
+
+    def _degitize_all(self, examples):
+        degitized_examples = []
+        for (words, tags) in examples:
+            word_indexes = self._degitize(words, self.word_to_index)
+            tag_indexes = self._degitize(tags, self.tag_to_index)
+            degitized_examples.append((word_indexes, tag_indexes))
+        return degitized_examples
+
+    def _degitize(self, tokens, token_to_index):
+        indexes = [token_to_index[token] for token in tokens]
+        return torch.tensor(indexes, dtype=torch.long)
---
+++ tests/test_pos_tagging_dataset.py
@@ -0,0 +1,33 @@
+import unittest
+import torch
+from bseg.pos_tagging_dataset import PosTaggingDataset
+
+
+class TestPosTaggingDataset(unittest.TestCase):
+
+    def setUp(self):
+        self.examples = [(("人参", "を", "切る"), ("名詞", "助詞", "動詞")),
+                         (("大根", "も", "切る"), ("名詞", "助詞", "動詞"))]
+        self.dataset = PosTaggingDataset(self.examples)
+
+    def test__make_index(self):
+        word_to_index, tag_to_index = self.dataset._make_index(self.examples)
+        self.assertEqual(word_to_index,
+                         {"人参": 0, "を": 1, "切る": 2, "大根": 3, "も": 4})
+        self.assertEqual(tag_to_index, {"名詞": 0, "助詞": 1, "動詞": 2})
+
+    def test__degitize_all(self):
+        word_to_index, tag_to_index = self.dataset._make_index(self.examples)
+        degitized_examples = self.dataset._degitize_all(self.examples)
+        self.assertTrue(torch.equal(degitized_examples[0][0],
+                                    torch.tensor([0, 1, 2], dtype=torch.long)))
+        self.assertTrue(torch.equal(degitized_examples[0][1],
+                                    torch.tensor([0, 1, 2], dtype=torch.long)))
+        self.assertTrue(torch.equal(degitized_examples[1][0],
+                                    torch.tensor([3, 4, 2], dtype=torch.long)))
+        self.assertTrue(torch.equal(degitized_examples[1][1],
+                                    torch.tensor([0, 1, 2], dtype=torch.long)))
+
+
+if __name__ == "__main__":
+    unittest.main()
