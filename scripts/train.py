867256bfb5f143049ee0975c2cb1f708f670327e
Jun Harashima <jun-harashima@cookpad.com>
2018-12-02 21:15:23 +0900

    modify train.py

--- scripts/train.py
+++ scripts/train.py
@@ -12,16 +12,6 @@ examples = [
 ]
 dataset = PosTaggingDataset(examples)
 
-model = Model(dataset, EMBEDDING_DIM, HIDDEN_DIM)
-model.train()
-
-with torch.no_grad():
-    word_indexes = dataset._degitize(examples[0][0], dataset.word_to_index)
-    scores = model(word_indexes)
-    # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
-    # for word i. The predicted tag is the maximum scoring tag.
-    # Here, we can see the predicted sequence below is 0 1 2 0 1
-    # since 0 is index of the maximum value of row 1,
-    # 1 is the index of maximum value of row 2, etc.
-    # Which is DET NOUN VERB DET NOUN, the correct sequence!
-    print(scores)
+model = Model(EMBEDDING_DIM, HIDDEN_DIM, len(dataset.word_to_index),
+              len(dataset.tag_to_index))
+model.train(dataset)
