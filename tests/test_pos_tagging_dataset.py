d3e6746c4337bc82811638f2c0c505209f8e1ec9
Jun Harashima <jun-harashima@cookpad.com>
2018-12-02 19:27:16 +0900

    consider PAD

--- tests/test_pos_tagging_dataset.py
+++ tests/test_pos_tagging_dataset.py
@@ -7,27 +7,27 @@ class TestPosTaggingDataset(unittest.TestCase):
 
     def setUp(self):
         self.examples = [(("人参", "を", "切る"), ("名詞", "助詞", "動詞")),
-                         (("大根", "も", "切る"), ("名詞", "助詞", "動詞"))]
+                         (("大根", "を", "切る"), ("名詞", "助詞", "動詞"))]
         self.dataset = PosTaggingDataset(self.examples)
 
     def test__make_index(self):
         word_to_index, tag_to_index = self.dataset._make_index(self.examples)
         self.assertEqual(word_to_index,
-                         {"人参": 0, "を": 1, "切る": 2, "大根": 3, "も": 4})
+                         {"PAD": 0,"人参": 1, "を": 2, "切る": 3, "大根": 4})
         self.assertEqual(tag_to_index,
-                         {"名詞": 0, "助詞": 1, "動詞": 2})
+                         {"PAD": 0, "名詞": 1, "助詞": 2, "動詞": 3})
 
     def test__degitize_all(self):
         word_to_index, tag_to_index = self.dataset._make_index(self.examples)
         degitized_examples = self.dataset._degitize_all(self.examples)
         self.assertTrue(torch.equal(degitized_examples[0][0],
-                                    torch.tensor([0, 1, 2], dtype=torch.long)))
+                                    torch.tensor([1, 2, 3], dtype=torch.long)))
         self.assertTrue(torch.equal(degitized_examples[0][1],
-                                    torch.tensor([0, 1, 2], dtype=torch.long)))
+                                    torch.tensor([1, 2, 3], dtype=torch.long)))
         self.assertTrue(torch.equal(degitized_examples[1][0],
-                                    torch.tensor([3, 4, 2], dtype=torch.long)))
+                                    torch.tensor([4, 2, 3], dtype=torch.long)))
         self.assertTrue(torch.equal(degitized_examples[1][1],
-                                    torch.tensor([0, 1, 2], dtype=torch.long)))
+                                    torch.tensor([1, 2, 3], dtype=torch.long)))
 
 
 if __name__ == "__main__":
