19c9a8970eac467ac65c2d6882761bb949b34c94
Jun Harashima <jun-harashima@cookpad.com>
2018-12-04 22:13:22 +0900

    small modification

--- scripts/train.py
+++ scripts/train.py
@@ -1,16 +1,16 @@
 import torch
 from model import Model
-from pos_tagging_dataset import PosTaggingDataset
+from dataset import Dataset
 
 
 EMBEDDING_DIM = 6
 HIDDEN_DIM = 6
 
-examples = [
-    ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
-    ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
-]
-dataset = PosTaggingDataset(examples)
+examples = [(("人参", "を", "切る"), ("名詞", "助詞", "動詞")),
+            (("大根", "を", "切る"), ("名詞", "助詞", "動詞")),
+            (("白菜", "は", "蒸す"), ("名詞", "助詞", "動詞")),
+            (("牛蒡", "は", "削ぐ"), ("名詞", "助詞", "動詞"))]
+dataset = Dataset(examples)
 
 model = Model(EMBEDDING_DIM, HIDDEN_DIM, len(dataset.word_to_index),
               len(dataset.tag_to_index))
